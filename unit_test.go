08768040a9f358c450729f11b080874b628c6b43
Ludi Rehak <ludi317@gmail.com>
2022-11-07 16:31:17 -0800

    Improvements to the testing framework: 1) Complete input space coverage. Test every possible pair of (hi, lo) bytes to blitz through all combinations = (1 << 8 * 1 << 8 = 1 << 16). This means fewer test iterations with no loss in coverage (and possibly a gain) as compared to randomly generating a million bytes to decode. 2) Use a parallelized Runner to call all From() variants in the same unit test. It's faster, because it's parallel, and more concise, because it avoids code duplication, among other vices.

--- unit_test.go
+++ unit_test.go
@@ -2,217 +2,50 @@ package hex_decoding
 
 import (
 	"encoding/hex"
-	"math/rand"
+	"reflect"
+	"runtime"
 	"testing"
 )
 
-func TestFrom2DInt16(t *testing.T) {
-	// construct 2*1MB of hex encoded input, 1MB max (big enough to exceed L1D; small enough to fit in L3)
-	bin := make([]byte, 1_000_000)
-	rand.Read(bin)
-	hexStr := hex.EncodeToString(bin)
+func TestFrom(t *testing.T) {
+	for _, from := range []func(hi, lo byte, ok bool) (byte, bool){
+		From2DInt16,
+		From1DInt16,
+		From2Dbyte,
+		From2SmallArrays,
+		FromSmallString,
+		FromBigString,
+		From2SmallStrings,
+		From1Dbyte,
+		FromMath,
+		FromBranching,
+	} {
+		t.Run(getFunctionName(from), func(t *testing.T) {
+			t.Parallel()
+			for hi := 0; hi < 1<<8; hi++ {
+				for lo := 0; lo < 1<<8; lo++ {
+					gotVal, ok := from(byte(hi), byte(lo), true)
+					// compare our result with the std library's
+					wantVal, err := hex.DecodeString(string([]byte{byte(hi), byte(lo)}))
+					if ok {
+						if err != nil {
+							t.Fatalf("want: invalid hex, got: valid hex for hi: %v, lo: %v for %s", hi, lo, t.Name())
+						}
+						if gotVal != wantVal[0] {
+							t.Fatalf("want: %v, got: %v for hi: %v, lo: %v for %s", wantVal, gotVal, hi, lo, t.Name())
+						}
+					} else { // !ok
+						if err == nil {
+							t.Fatalf("want: valid hex, got: invalid hex for hi: %v, lo: %v for %s", hi, lo, t.Name())
+						}
+					}
+				}
+			}
+		})
+	}
 
-	// decode the hex we just created, and check it matches bin
-	for j := 0; j < len(hexStr); j += 2 {
-		hi, lo := hexStr[j], hexStr[j+1]
-		h, ok := From2DInt16(hi, lo, true)
-		if !ok || bin[j/2] != h {
-			t.Fatalf("From(%c, %c) returned (0x%x, %v), expected 0x%x", hi, lo, h, ok, bin[j/2])
-		}
-	}
-	// negative cases
-	_, ok := From2DInt16('l', 'r', true)
-	if ok {
-		t.Fatalf("want not hex, got hex")
-	}
 }
 
-func TestFrom1DInt16(t *testing.T) {
-	// construct 2*1MB of hex encoded input, 1MB max (big enough to exceed L1D; small enough to fit in L3)
-	bin := make([]byte, 1_000_000)
-	rand.Read(bin)
-	hexStr := hex.EncodeToString(bin)
-
-	// decode the hex we just created, and check it matches bin
-	for j := 0; j < len(hexStr); j += 2 {
-		hi, lo := hexStr[j], hexStr[j+1]
-		h, ok := From1DInt16(hi, lo, true)
-		if !ok || bin[j/2] != h {
-			t.Fatalf("From(%c, %c) returned (0x%x, %v), expected 0x%x", hi, lo, h, ok, bin[j/2])
-		}
-	}
-	// negative cases
-	_, ok := From1DInt16('l', 'r', true)
-	if ok {
-		t.Fatalf("want not hex, got hex")
-	}
-}
-
-func TestFrom2Dbyte(t *testing.T) {
-	// construct 2*1MB of hex encoded input, 1MB max (big enough to exceed L1D; small enough to fit in L3)
-	bin := make([]byte, 1_000_000)
-	rand.Read(bin)
-	hexStr := hex.EncodeToString(bin)
-
-	// decode the hex we just created, and check it matches bin
-	for j := 0; j < len(hexStr); j += 2 {
-		hi, lo := hexStr[j], hexStr[j+1]
-		h, ok := From2Dbyte(hi, lo, true)
-		if !ok || bin[j/2] != h {
-			t.Fatalf("From(%c, %c) returned (0x%x, %v), expected 0x%x", hi, lo, h, ok, bin[j/2])
-		}
-	}
-	// negative cases
-	_, ok := From2Dbyte('l', 'r', true)
-	if ok {
-		t.Fatalf("want not hex, got hex")
-	}
-}
-
-func TestFrom2SmallArrays(t *testing.T) {
-	// construct 2*1MB of hex encoded input, 1MB max (big enough to exceed L1D; small enough to fit in L3)
-	bin := make([]byte, 1_000_000)
-	rand.Read(bin)
-	hexStr := hex.EncodeToString(bin)
-
-	// decode the hex we just created, and check it matches bin
-	for j := 0; j < len(hexStr); j += 2 {
-		hi, lo := hexStr[j], hexStr[j+1]
-		h, ok := From2SmallArrays(hi, lo, true)
-		if !ok || bin[j/2] != h {
-			t.Fatalf("From(%c, %c) returned (0x%x, %v), expected 0x%x", hi, lo, h, ok, bin[j/2])
-		}
-	}
-
-	// negative cases
-	_, ok := From2SmallArrays('l', 'r', true)
-	if ok {
-		t.Fatalf("want not hex, got hex")
-	}
-}
-
-func TestFromSmallString(t *testing.T) {
-	// construct 2*1MB of hex encoded input, 1MB max (big enough to exceed L1D; small enough to fit in L3)
-	bin := make([]byte, 1_000_000)
-	rand.Read(bin)
-	hexStr := hex.EncodeToString(bin)
-
-	// decode the hex we just created, and check it matches bin
-	for j := 0; j < len(hexStr); j += 2 {
-		hi, lo := hexStr[j], hexStr[j+1]
-		h, ok := FromSmallString(hi, lo, true)
-		if !ok || bin[j/2] != h {
-			t.Fatalf("From(%c, %c) returned (0x%x, %v), expected 0x%x", hi, lo, h, ok, bin[j/2])
-		}
-	}
-	// negative cases
-	_, ok := FromSmallString('l', 'r', true)
-	if ok {
-		t.Fatalf("want not hex, got hex")
-	}
-}
-
-func TestFromBigString(t *testing.T) {
-	// construct 2*1MB of hex encoded input, 1MB max (big enough to exceed L1D; small enough to fit in L3)
-	bin := make([]byte, 1_000_000)
-	rand.Read(bin)
-	hexStr := hex.EncodeToString(bin)
-
-	// decode the hex we just created, and check it matches bin
-	for j := 0; j < len(hexStr); j += 2 {
-		hi, lo := hexStr[j], hexStr[j+1]
-		h, ok := FromBigString(hi, lo, true)
-		if !ok || bin[j/2] != h {
-			t.Fatalf("From(%c, %c) returned (0x%x, %v), expected 0x%x", hi, lo, h, ok, bin[j/2])
-		}
-	}
-	// negative cases
-	_, ok := FromBigString('l', 'r', true)
-	if ok {
-		t.Fatalf("want not hex, got hex")
-	}
-}
-
-func TestFrom2SmallStrings(t *testing.T) {
-	// construct 2*1MB of hex encoded input, 1MB max (big enough to exceed L1D; small enough to fit in L3)
-	bin := make([]byte, 1_000_000)
-	rand.Read(bin)
-	hexStr := hex.EncodeToString(bin)
-
-	// decode the hex we just created, and check it matches bin
-	for j := 0; j < len(hexStr); j += 2 {
-		hi, lo := hexStr[j], hexStr[j+1]
-		h, ok := From2SmallStrings(hi, lo, true)
-		if !ok || bin[j/2] != h {
-			t.Fatalf("From(%c, %c) returned (0x%x, %v), expected 0x%x", hi, lo, h, ok, bin[j/2])
-		}
-	}
-	// negative cases
-	_, ok := From2SmallStrings('l', 'r', true)
-	if ok {
-		t.Fatalf("want not hex, got hex")
-	}
-}
-
-func TestFrom1Dbyte(t *testing.T) {
-	// construct 2*1MB of hex encoded input, 1MB max (big enough to exceed L1D; small enough to fit in L3)
-	bin := make([]byte, 1_000_000)
-	rand.Read(bin)
-	hexStr := hex.EncodeToString(bin)
-
-	// decode the hex we just created, and check it matches bin
-	for j := 0; j < len(hexStr); j += 2 {
-		hi, lo := hexStr[j], hexStr[j+1]
-		h, ok := From1Dbyte(hi, lo, true)
-		if !ok || bin[j/2] != h {
-			t.Fatalf("From(%c, %c) returned (0x%x, %v), expected 0x%x", hi, lo, h, ok, bin[j/2])
-		}
-	}
-	// negative cases
-	_, ok := From1Dbyte('l', 'r', true)
-	if ok {
-		t.Fatalf("want not hex, got hex")
-	}
-}
-
-func TestFromMath(t *testing.T) {
-	// construct 2*1MB of hex encoded input, 1MB max (big enough to exceed L1D; small enough to fit in L3)
-	bin := make([]byte, 1_000_000)
-	rand.Read(bin)
-	hexStr := hex.EncodeToString(bin)
-
-	// decode the hex we just created, and check it matches bin
-	for j := 0; j < len(hexStr); j += 2 {
-		hi, lo := hexStr[j], hexStr[j+1]
-		h, ok := FromMath(hi, lo, true)
-		if !ok || bin[j/2] != h {
-			t.Fatalf("From(%c, %c) returned (0x%x, %v), expected 0x%x", hi, lo, h, ok, bin[j/2])
-		}
-	}
-	// negative cases
-	_, ok := FromMath('l', 'r', true)
-	if ok {
-		t.Fatalf("want not hex, got hex")
-	}
-}
-
-func TestFromBranching(t *testing.T) {
-	// construct 2*1MB of hex encoded input, 1MB max (big enough to exceed L1D; small enough to fit in L3)
-	bin := make([]byte, 1_000_000)
-	rand.Read(bin)
-	hexStr := hex.EncodeToString(bin)
-
-	// decode the hex we just created, and check it matches bin
-	for j := 0; j < len(hexStr); j += 2 {
-		hi, lo := hexStr[j], hexStr[j+1]
-		h, ok := FromBranching(hi, lo, true)
-		if !ok || bin[j/2] != h {
-			t.Fatalf("From(%c, %c) returned (0x%x, %v), expected 0x%x", hi, lo, h, ok, bin[j/2])
-		}
-	}
-	// negative cases
-	_, ok := FromBranching('l', 'r', true)
-	if ok {
-		t.Fatalf("want not hex, got hex")
-	}
+func getFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
 }
